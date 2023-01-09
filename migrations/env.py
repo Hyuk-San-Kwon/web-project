2fb3c8cef296c60655bb86a2c5946abb5b0182f5
Kwon <rnjsgurtkswk@gmail.com>
2023-01-10 00:29:40 +0900

    3.03

--- migrations/env.py
+++ migrations/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import models
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -7,8 +8,6 @@ from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-import models
-
 config = context.config
 
 # Interpret the config file for Python logging.
@@ -21,7 +20,6 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = models.Base.metadata
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
